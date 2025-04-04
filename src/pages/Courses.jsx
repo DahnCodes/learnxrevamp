103dd83cb4440c524df317c9bd2e8e3f84e4c146
DahnCodes <stanleydaniel994@gmail.com>
2025-04-04 02:30:28 +0100

    update3.0

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,5 +1,6 @@
 import  { useState, useEffect } from 'react';
 import axios from 'axios';
+import "../styles/Newcourse.css";
 
 const CoursePage = () => {
   const [courses, setCourses] = useState([]);
